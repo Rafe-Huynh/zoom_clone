2e4b83bba5216d2c1402611304a4397e6dbf2680
Thai Huynh <rafehuynh0510@gmail.com>
2024-07-04 19:45:29 -0500

    Clerk Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider} from '@clerk/nextjs'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +15,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider
+    appearance={{
+      layout: {
+        socialButtonsVariant: 'iconButton'
+      },
+      variables:{
+        colorText: 'black',
+        colorPrimary: '#0E78F9',
+        colorInputBackground: '#252a41',
+        colorInputText: '#fff'
+      }
+    }}>
     <html lang="en">
       <body className={`${inter.className} bg-dark-2`}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
